afb18fe8fbf6c659b35b3c6e4169ed418f4b4bb5
Mark Silverio <dashplaygrounds@gmail.com>
2023-09-18 11:57:38 +0800

    (master) v1.0.0 - feat: blogapp initial commit

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/usr/local/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/admin/projects/BlogApp"
+export "FLUTTER_APPLICATION_PATH=/home/admin/projects/flutter_blog_app"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
